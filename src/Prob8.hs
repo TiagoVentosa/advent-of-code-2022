106bcada8b7257a0d81e6f622ba172b1c7d44520
Tiago Ventosa <tiago.ventosa@vwds.pt>
2022-12-09 18:54:01 +0000

    :sparkles: Problem 8 Part 2

--- src/Prob8.hs
+++ src/Prob8.hs
@@ -4,6 +4,7 @@ import Text.Megaparsec
 import Data.Void (Void)
 import Text.Megaparsec.Char (newline)
 import Data.List (singleton, transpose)
+import Data.Bifunctor (bimap)
 
 type Parser = Parsec Void String
 
@@ -23,3 +24,22 @@ toVisibleTrees trees =
   let rightVisible = zipWith (>) trees $ tail $ scanr max (-1) trees
       leftVisible = zipWith (>) trees $ init $ scanl max (-1) trees
   in zipWith (||) rightVisible leftVisible
+  
+solution' :: [[Int]] -> Int
+solution' treesMatrix =
+  let linesVisible = map toScenicScore treesMatrix
+      colsVisible = transpose $ map toScenicScore $ transpose treesMatrix
+      visibleMatrix = zipWith (zipWith (*)) linesVisible colsVisible
+  in maximum $ map maximum visibleMatrix
+
+toScenicScore :: [Int] -> [Int]
+toScenicScore trees =
+  let rightScore = go trees
+      leftScore = reverse . go . reverse $ trees
+  in zipWith (*) rightScore leftScore
+  where go []     = []
+        go (t:ts) = scoreTree t ts : go ts
+        scoreTree t ts = 
+          uncurry (+) $ 
+          bimap length (fromEnum . (/=[])) $ 
+          span (<t) ts
