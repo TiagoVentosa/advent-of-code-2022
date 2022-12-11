5314b6d2e91420daa90aa650812a549fa766223a
Tiago Ventosa <tiago.ventosa@vwds.pt>
2022-12-11 21:41:45 +0000

    :sparkles: Problem 11 Part 1

---
+++ src/Prob11.hs
@@ -0,0 +1,91 @@
+module Prob11 where
+
+import Data.Void (Void)
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Char (newline, string, space)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Data.Functor ((<&>), ($>))
+import Control.Monad.State (State, evalState, gets, modify, replicateM)
+import qualified Data.Sequence as S
+import Data.List (partition, sortOn)
+import Data.Ord (Down(..))
+
+type Parser = Parsec Void String
+data Monkey = Monkey Operation ThrowTest deriving (Eq, Show)
+data Operation = Add Int | Mult Int | Sqr deriving (Eq, Show)
+data ThrowTest = ThrowTest Int Int Int deriving (Eq, Show)
+
+restOfLine :: Parser String
+restOfLine = many (anySingleBut '\n')
+
+inputParser :: Parser [([Int],Monkey)]
+inputParser = parseMonkey `sepBy` (newline <* newline)
+
+parseMonkey :: Parser ([Int], Monkey)
+parseMonkey =
+  (,) <$ restOfLine <* newline
+  <*> parseStartingItems <* newline
+  <*> (
+    Monkey <$> parseOperation <* newline
+    <*> parseThrowTest
+  )
+
+parseStartingItems :: Parser [Int]
+parseStartingItems =
+  space *> string "Starting items: " *> decimal `sepBy` string ", "
+
+parseOperation :: Parser Operation
+parseOperation =
+  space *> string "Operation: new = old " *>
+  ((string "+ " *> decimal <&> Add)
+  <|>
+  (string "* " *> (
+      (decimal <&> Mult)
+      <|>
+      (string "old" $> Sqr)
+    )
+  ))
+
+parseThrowTest :: Parser ThrowTest
+parseThrowTest =
+  ThrowTest
+  <$ space <* string "Test: divisible by " <*> decimal <* newline
+  <* space <* string "If true: throw to monkey " <*> decimal <* newline
+  <* space <* string "If false: throw to monkey " <*> decimal
+
+
+solution :: [([Int], Monkey)] -> Int
+solution monkeysWithStartingItems = let
+  initialConditions = S.fromList $ fmap fst monkeysWithStartingItems
+  monkeys = fmap snd monkeysWithStartingItems
+  in product $ take 2 $ sortOn Down $ evalState (playRounds monkeys) initialConditions
+
+playRounds :: [Monkey] -> State (S.Seq [Int]) [Int]
+playRounds monkeys = foldr1 (zipWith (+)) <$> replicateM 20 (playRound monkeys)
+  
+playRound :: [Monkey] -> State (S.Seq [Int]) [Int]
+playRound monkeys = do
+  let monkeysWithNm = zip ([0..]::[Int]) monkeys
+      plays = fmap playTurn monkeysWithNm
+  sequence plays
+
+playTurn :: (Int, Monkey) -> State (S.Seq [Int]) Int
+playTurn (nm, Monkey operation test) = do
+  items <- gets (`S.index` nm)
+  let operatedItems = fmap ((`div` 3) . doOperation operation) items
+  sendToOtherMonkeys test operatedItems
+  modify (S.update nm [])
+  pure $ length items
+
+doOperation :: Operation -> Int -> Int
+doOperation (Add a) v = v + a
+doOperation (Mult m) v = v * m
+doOperation Sqr v = v * v
+
+sendToOtherMonkeys :: ThrowTest -> [Int] -> State (S.Seq [Int]) ()
+sendToOtherMonkeys 
+  (ThrowTest divisable trueTarget falseTarget)
+  items = do 
+    let (passed, failed) = partition ((==0) . (`mod` divisable)) items
+    modify (S.adjust (++ passed) trueTarget)
+    modify (S.adjust (++ failed) falseTarget)
