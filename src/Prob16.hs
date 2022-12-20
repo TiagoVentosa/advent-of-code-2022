869b89fc2a735b66fc0b4d867a1c83bdae510c05
Tiago Ventosa <tiago.ventosa@vwds.pt>
2022-12-20 03:21:45 +0000

    :sparkles: Problem 16 part 2

--- src/Prob16.hs
+++ src/Prob16.hs
@@ -11,7 +11,7 @@ import qualified Data.List.NonEmpty as NE
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Prob16.Minpath
-import Data.List (singleton)
+import Data.List (singleton, intersect)
 
 type Parser = Parsec Void String
 type ValveName = (Char, Char)
@@ -46,6 +46,17 @@ solution input = let
   where toValvePair (name, rate, others) = (name, Valve rate (fmap (,1) others) )
         first (name, _, _) = name
 
+solution' :: [(ValveName, Int, NonEmpty ValveName)] -> Int
+solution' input = let
+  valvesMap = M.fromList $ fmap toValvePair input
+  usefulValves = map first $ filter (\(_, rate, _) -> rate /= 0) input
+  completeValveMap = fillPaths valvesMap usefulValves
+  routes = calculatePressureForRoutes' usefulValves completeValveMap
+  in getBestDoubleRoute routes
+  where toValvePair (name, rate, others) = (name, Valve rate (fmap (,1) others) )
+        first (name, _, _) = name
+
+
 startingValve :: ValveName
 startingValve = ('A', 'A')
 
@@ -60,7 +71,7 @@ fillPaths valvesMap usefulValves = foldr addPathsToMap valvesMap (startingValve 
           in updatedMap'
         getNeighbourValves name = fmap fst $ NE.toList $ valveTunnels $ valvesMap M.! name
         updateOtherTunnel name (name', dist) mapToUpdate' = M.update (Just . appendTunnels (singleton (name, dist))) name' mapToUpdate'
-        
+
 appendTunnels :: [(ValveName, Int)] -> Valve -> Valve
 appendTunnels newTunnels (Valve rate tunnels) = Valve rate (NE.appendList tunnels newTunnels)
 
@@ -70,20 +81,52 @@ calculatePressureForRoutes usefulValves valvesMap =
   where go (Left finalValue) = pure finalValue
         go currState =
           calculateNextValve usefulValves valvesMap currState >>= go
-      
-calculateNextValve :: [ValveName] -> Map ValveName Valve -> 
-                      Either Int (NonEmpty ValveName, Int, Int) -> 
+
+calculatePressureForRoutes' :: [ValveName] -> Map ValveName Valve -> [([ValveName], Int)]
+calculatePressureForRoutes' usefulValves valvesMap =
+  go $ Right (NE.singleton startingValve, 26, 0)
+  where go (Left finalValue) = pure finalValue
+        go currState =
+          calculateNextValve' usefulValves valvesMap currState >>= go
+
+
+calculateNextValve :: [ValveName] -> Map ValveName Valve ->
+                      Either Int (NonEmpty ValveName, Int, Int) ->
                       [Either Int (NonEmpty ValveName, Int, Int)]
 calculateNextValve _ _ (Left finalValue) = pure $ Left finalValue
 calculateNextValve usefulValves valvesMap (Right (visitedValves@(currValve :| _), timeLeft, pressureReleased)) = let
   nextValves = filter (`notElem` visitedValves) usefulValves
-  nextValvesWithDistance = filter ((>= 0) . newTimeLeft . snd) $ 
+  nextValvesWithDistance = filter ((>= 0) . newTimeLeft . snd) $
     NE.filter ((`elem` nextValves) . fst) $ valveTunnels $ valvesMap M.! currValve
   in case nextValvesWithDistance of
     [] -> pure $ Left pressureReleased
     _ -> fmap nextState nextValvesWithDistance
   where newTimeLeft dist = timeLeft - dist - 1
-        nextState (valve, dist) = 
+        nextState (valve, dist) =
           Right (valve <| visitedValves,
-          newTimeLeft dist, 
+          newTimeLeft dist,
           (+ pressureReleased) $ (* newTimeLeft dist) $ valveRate $ valvesMap M.! valve)
+
+calculateNextValve' :: [ValveName] -> Map ValveName Valve ->
+                      Either ([ValveName], Int) (NonEmpty ValveName, Int, Int) ->
+                      [Either ([ValveName], Int) (NonEmpty ValveName, Int, Int)]
+calculateNextValve' _ _ (Left finalValue) = pure $ Left finalValue
+calculateNextValve' usefulValves valvesMap (Right (visitedValves@(currValve :| _), timeLeft, pressureReleased)) = let
+  nextValves = filter (`notElem` visitedValves) usefulValves
+  nextValvesWithDistance = filter ((>= 0) . newTimeLeft . snd) $
+    NE.filter ((`elem` nextValves) . fst) $ valveTunnels $ valvesMap M.! currValve
+  in case nextValvesWithDistance of
+    [] -> pure $ Left (NE.init visitedValves, pressureReleased)
+    _ -> Left (NE.init visitedValves, pressureReleased) : fmap nextState nextValvesWithDistance
+  where newTimeLeft dist = timeLeft - dist - 1
+        nextState (valve, dist) =
+          Right (valve <| visitedValves,
+          newTimeLeft dist,
+          (+ pressureReleased) $ (* newTimeLeft dist) $ valveRate $ valvesMap M.! valve)
+
+getBestDoubleRoute :: [([ValveName], Int)] -> Int
+getBestDoubleRoute routes =
+  foldr go 0 routes
+  where go (route, press) prevMax = let
+          routeMax = maximum $ (0:) $ map snd $ filter (null . (`intersect` route) . fst) $ filter ((> (prevMax - press)) . snd) routes
+          in max prevMax (press + routeMax)
